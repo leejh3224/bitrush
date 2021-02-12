24c7b3b878208279460d9dfb19bde9d53876f55d
gompro <junhyung3224@gmail.com>
2021-02-12 23:32:49 +0900

    Refactoring

--- trader.py
+++ trader.py
@@ -9,7 +9,7 @@ from lib.strategies.aroon import Aroon
 from lib.strategies.rsi_bb import RsiBB
 from lib.broker import Broker
 from lib.upbit import Upbit
-from lib.strategies.base_strategy import StrategyParams
+from lib.strategies.base_strategy import BaseStrategy, StrategyParams
 from decimal import *
 from lib.ticker import Ticker
 from lib.sqs import order_queue
@@ -19,31 +19,31 @@ broker = Broker(api, order_queue)
 
 
 def main(event, context):
-    GoldenCross(
-        broker, StrategyParams(ticker=Ticker.이더리움.value, ratio=Decimal(0.2))
-    ).trade()
-
-    VolatilityBreakout(
-        broker,
-        StrategyParams(
-            ticker=Ticker.비트코인.value,
-            ratio=Decimal(0.2),
+    strategies: list[BaseStrategy] = [
+        GoldenCross(
+            broker, StrategyParams(ticker=Ticker.이더리움.value, ratio=Decimal(0.2))
         ),
-    ).trade()
-
-    VolatilityBreakout(
-        broker,
-        StrategyParams(
-            ticker=Ticker.이더리움.value,
-            ratio=Decimal(0.2),
+        VolatilityBreakout(
+            broker,
+            StrategyParams(
+                ticker=Ticker.비트코인.value,
+                ratio=Decimal(0.2),
+            ),
         ),
-    ).trade()
+        VolatilityBreakout(
+            broker,
+            StrategyParams(
+                ticker=Ticker.이더리움.value,
+                ratio=Decimal(0.2),
+            ),
+        ),
+        StochRSI(broker, StrategyParams(ticker=Ticker.리플.value, ratio=Decimal(0.2))),
+        Aroon(broker, StrategyParams(ticker=Ticker.비트코인.value, ratio=Decimal(0.2))),
+        RsiBB(broker, StrategyParams(ticker=Ticker.이더리움.value, ratio=Decimal(0.2))),
+    ]
 
-    StochRSI(broker, StrategyParams(ticker=Ticker.리플.value, ratio=Decimal(0.2))).trade()
-
-    Aroon(broker, StrategyParams(ticker=Ticker.비트코인.value, ratio=Decimal(0.2))).trade()
-
-    RsiBB(broker, StrategyParams(ticker=Ticker.이더리움.value, ratio=Decimal(0.2))).trade()
+    for strategy in strategies:
+        strategy.trade()
 
 
 if __name__ == "__main__":
