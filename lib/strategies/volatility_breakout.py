e5094a43b3d2c9b0b454f789761d5b66a78ec597
gompro <junhyung3224@gmail.com>
2021-02-14 23:41:12 +0900

    Add stop loss condition for vb

--- lib/strategies/volatility_breakout.py
+++ lib/strategies/volatility_breakout.py
@@ -8,13 +8,17 @@ class VolatilityBreakout(BaseStrategy):
 
     # settings
     k = Decimal(0.5)
+    stop_loss_threshold = Decimal(0.1)
 
-    def should_buy(self):
+    def __init__(self, broker, params) -> None:
+        super().__init__(broker, params)
         ticker = self.params["ticker"]
 
-        feed = self.broker.get_feed(ticker)
-        yesterday_ohlcv = feed.iloc[-2]
-        current_ohlcv = feed.iloc[-1]
+        self.feed = self.broker.get_feed(ticker)
+
+    def should_buy(self):
+        yesterday_ohlcv = self.feed.iloc[-2]
+        current_ohlcv = self.feed.iloc[-1]
         range = yesterday_ohlcv["high"] - yesterday_ohlcv["low"]
 
         return current_ohlcv["close"] > current_ohlcv["open"] + (range * self.k)
@@ -23,4 +27,19 @@ class VolatilityBreakout(BaseStrategy):
         now = datetime.now()
         sell_period = datetime(now.year, now.month, now.day, 6, 0, 0)
 
-        return sell_period < now <= sell_period + timedelta(minutes=20)
+        close_when = sell_period < now <= sell_period + timedelta(minutes=20)
+        stop_loss = False
+
+        ticker = self.params["ticker"]
+        last_trade = self.broker.get_last_trade(ticker=ticker, strategy=self.name)
+        current_ohlcv = self.feed.iloc[-1]
+
+        if last_trade.trade_type:
+            avg_price = last_trade.avg_price
+            stop_loss = (
+                avg_price - current_ohlcv["close"]
+                > avg_price * self.stop_loss_threshold
+            )
+
+        # 평단가 기준으로 손절하거나 다음날 포지션 청산
+        return stop_loss or close_when
