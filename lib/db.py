380c64c73247916e9f0f3f14119f66caf744c59f
gompro <junhyung3224@gmail.com>
2021-02-14 10:49:33 +0900

    Turn off sql logging on prod

--- lib/db.py
+++ lib/db.py
@@ -2,13 +2,14 @@ import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 import os
 
+sql_logging = os.getenv("SQL_LOGGING")
 connection_string = os.getenv("BITRUSH_CONNECTION_STRING")
 
 engine = db.create_engine(
     connection_string,
     pool_size=20,
     convert_unicode=True,
-    echo=True,
+    echo=sql_logging == "True",
 )
 Session = sessionmaker(bind=engine)
 session = Session()
